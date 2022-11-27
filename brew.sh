04ae659b3976736e3bdd24685bc74911e9a4c7f0
Sushil Nagi <sushil.nagi@gmail.com>
2022-11-27 13:19:40 +0000

    Install brew if not already installed

--- brew.sh
+++ brew.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Install brew if not installed
+if ! command -v brew &> /dev/null
+then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 # Install command-line tools using Homebrew.
 
 # Make sure we’re using the latest Homebrew.
